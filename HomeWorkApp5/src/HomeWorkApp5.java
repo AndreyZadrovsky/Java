2ab86e8931d7134add408fbc1e5ee373536cc85f
Andrey <andr-zadr@yandex.ru>
2021-06-01 17:15:07 +0300

    update

--- HomeWorkApp5/src/HomeWorkApp5.java
+++ HomeWorkApp5/src/HomeWorkApp5.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class HomeWorkApp5 {
     public static void main(String[] args) {
         Employee[] empArray = new Employee[5];
@@ -6,10 +8,14 @@ public class HomeWorkApp5 {
         empArray[2] = new Employee("Ватрушкин Иван Игоревич", "менеджер", "ivatr@yandex.ru", 89823686412L , 75000, 40);
         empArray[3] = new Employee("Жук Марина Романовна", "директор", "zhuk1967@mail.ru", 89816146378L , 120000, 45);
         empArray[4] = new Employee("Фирсова Светлана Андреевна", "бухгалтер", "firsovasvetlana@gmail.com", 89317357348L , 100000, 39);
+        System.out.println("От какого возраста включительно вывести на экран");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
         for (int i = 0; i < 5; i++){
-            if (empArray[i].age >= 40){
+            if (empArray[i].age >= a){
                 empArray[i].print();
             }
         }
     }
 }
+
