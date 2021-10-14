f4ee4346f1a7bfc4f5bd5640d579d74077672e77
Andrey <andr-zadr@yandex.ru>
2021-10-14 14:46:22 +0300

    Homework7

--- HomeWorkApp7/src/main/java/Main.java
+++ HomeWorkApp7/src/main/java/Main.java
@@ -1,3 +1,50 @@
-public class main {
-    Class class = Class.
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class cls = MyClass.class;
+        Object obj = cls.getDeclaredConstructor().newInstance();
+
+        Method[] methods = cls.getDeclaredMethods();
+        List<Method> listTest = new ArrayList<>();
+
+        Method before = null;
+        Method after = null;
+
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(BeforeSuite.class)) {
+                if(before == null){
+                    before = method;
+                }else throw new RuntimeException("Может быть только один такой метод");
+            }
+            if (method.isAnnotationPresent(AfterSuite.class)) {
+                if(after == null){
+                    after = method;
+                }else throw new RuntimeException("Может быть только один такой метод");
+            }
+            if (method.isAnnotationPresent(Test.class)){
+                listTest.add(method);
+            }
+        }
+        listTest.sort(new Comparator<Method>() {
+            @Override
+            public int compare(Method o1, Method o2) {
+                return o1.getAnnotation(Test.class).priority() - o2.getAnnotation(Test.class).priority();
+            }
+        });
+        if (before != null) {
+            listTest.add(0, before);
+        }
+        if (after != null) {
+            listTest.add(after);
+        }
+
+        for (Method method : listTest) {
+            method.invoke(obj,null);
+        }
+    }
 }
--- HomeWorkApp7/src/main/java/MyClass.java
+++ HomeWorkApp7/src/main/java/MyClass.java
@@ -1,4 +1,4 @@
-public class Class {
+public class MyClass {
 
     @BeforeSuite
     public void before(){
@@ -9,43 +9,43 @@ public class Class {
         System.out.println("Test1");
     }
 
-    @Test(priority = 1)
+    @Test(priority = 5)
     public void test2(){
         System.out.println("Test2");
     }
 
-    @Test(priority = 1)
+    @Test(priority = 2)
     public void test3(){
         System.out.println("Test3");
     }
 
-    @Test(priority = 1)
+    @Test(priority = 6)
     public void test4(){
         System.out.println("Test4");
     }
 
-    @Test(priority = 1)
+    @Test(priority = 3)
     public void test5(){
         System.out.println("Test5");
     }
 
-    @Test(priority = 1)
+    @Test(priority = 7)
     public void test6(){
         System.out.println("Test6");
     }
 
-    @Test(priority = 1)
+    @Test(priority = 4)
     public void test7(){
         System.out.println("Test7");
     }
 
-    @Test(priority = 1)
+    @Test(priority = 8)
     public void test8(){
         System.out.println("Test8");
     }
 
-    @BeforeSuite
+    @AfterSuite
     public void after() {
-        System.out.println("After");
+        System.out.println("AfterSuite");
                  }
 }
